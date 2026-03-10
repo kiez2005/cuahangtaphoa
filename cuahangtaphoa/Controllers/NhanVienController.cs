9f6574dcfffd524e4e211b85dd765a593721ca62
manhdat77 <manhdat772005@gmail.com>
2026-03-10 22:39:51 +0700

    Nhân viên + Layout

---
+++ cuahangtaphoa/Controllers/NhanVienController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using cuahangtaphoa.Models;
+
+namespace cuahangtaphoa.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        testEntities db = new testEntities();
+
+        // Hiển thị danh sách nhân viên
+        public ActionResult Index()
+        {
+            var ds = db.NguoiDungs.ToList();
+            return View(ds);
+        }
+
+        // Trang thêm nhân viên
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Lưu nhân viên
+        [HttpPost]
+        public ActionResult Create(NguoiDung nv)
+        {
+            db.NguoiDungs.Add(nv);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
